7655d12706ace223e5d7bcfccc5005b6dcb87002
ZayZayInTheLibrary <zayzaylibrary@yahoo.com>
2025-02-18 22:49:11 +0000

    main

---
+++ Name To Color/main.py
@@ -0,0 +1,21 @@
+from functools import reduce
+
+name = input("What is your name?\n")
+
+nameAscii = [ord(c) for c in name]
+nameAscii = list(map(int, nameAscii))
+print(nameAscii)
+
+nameAsciiSum = sum(nameAscii)
+print(nameAsciiSum)
+
+nameAsciiDif = reduce(lambda x, y: x - y, nameAscii)
+
+nameAsciiMult = reduce(lambda x, y: x * y, nameAscii)
+
+
+nameAsciiSum = nameAsciiSum % 256
+nameAsciiDif = nameAsciiDif % 256
+nameAsciiMult = nameAsciiMult % 256
+
+print(nameAsciiSum, nameAsciiDif, nameAsciiMult)
