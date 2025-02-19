eaaf355ab6f6a70e5155eab3de77cae911b06fab
ZayZayInTheLibrary <zayzaylibrary@yahoo.com>
2025-02-19 00:27:29 +0000

    main

--- Name To RGB/main.py
+++ Name To RGB/main.py
@@ -4,18 +4,23 @@ name = input("What is your name?\n")
 
 nameAscii = [ord(c) for c in name]
 nameAscii = list(map(int, nameAscii))
-print(nameAscii)
-
 nameAsciiSum = sum(nameAscii)
-print(nameAsciiSum)
-
 nameAsciiDif = reduce(lambda x, y: x - y, nameAscii)
-
 nameAsciiMult = reduce(lambda x, y: x * y, nameAscii)
-
-
 nameAsciiSum = nameAsciiSum % 256
 nameAsciiDif = nameAsciiDif % 256
 nameAsciiMult = nameAsciiMult % 256
 
 print(f"RGB: {nameAsciiSum, nameAsciiDif, nameAsciiMult}")
+
+def textToRGB(text):
+    name = text
+    nameAscii = [ord(c) for c in name]
+    nameAscii = list(map(int, nameAscii))
+    nameAsciiSum = sum(nameAscii)
+    nameAsciiDif = reduce(lambda x, y: x - y, nameAscii)
+    nameAsciiMult = reduce(lambda x, y: x * y, nameAscii)
+    nameAsciiSum = nameAsciiSum % 256
+    nameAsciiDif = nameAsciiDif % 256
+    nameAsciiMult = nameAsciiMult % 256
+    return (nameAsciiSum, nameAsciiDif, nameAsciiMult)
