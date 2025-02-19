d78bc0c2bec89c9ba1c7d61657e457151e1f8892
ZayZayInTheLibrary <zayzaylibrary@yahoo.com>
2025-02-19 00:15:11 +0000

    bot

--- Discord_Bot/main.py
+++ Discord_Bot/main.py
@@ -1,5 +1,11 @@
+import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+load_dotenv()
+
+token = os.getenv('DISCORD_TOKEN')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
