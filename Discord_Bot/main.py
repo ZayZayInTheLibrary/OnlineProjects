e9aa37d9ec1385958fa0f1253737c56a664dca38
ZayZayInTheLibrary <zayzaylibrary@yahoo.com>
2025-02-19 00:20:46 +0000

    main

--- Discord_Bot/main.py
+++ Discord_Bot/main.py
@@ -1,7 +1,7 @@
 import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 load_dotenv()
 
 token = os.getenv('DISCORD_TOKEN')
@@ -15,4 +15,4 @@ bot = commands.Bot(command_prefix='>', intents=intents)
 async def ping(ctx):
     await ctx.send('pong')
 
-bot.run('token')
+bot.run(token)
