fe4974bece0222be064fb1cdd8cf7a82140d390f
ZayZayInTheLibrary <zayzaylibrary@yahoo.com>
2025-02-24 23:58:10 +0000

    RGB

--- Name_To_RGB/mainRGB.py
+++ Name_To_RGB/mainRGB.py
@@ -1,17 +1,5 @@
 from functools import reduce
 
-name = input("What is your name?\n")
-
-nameAscii = [ord(c) for c in name]
-nameAscii = list(map(int, nameAscii))
-nameAsciiSum = sum(nameAscii)
-nameAsciiDif = reduce(lambda x, y: x - y, nameAscii)
-nameAsciiMult = reduce(lambda x, y: x * y, nameAscii)
-nameAsciiSum = nameAsciiSum % 256
-nameAsciiDif = nameAsciiDif % 256
-nameAsciiMult = nameAsciiMult % 256
-
-print(f"RGB: {nameAsciiSum, nameAsciiDif, nameAsciiMult}")
 
 def textToRGB(text):
     name = text
@@ -24,3 +12,7 @@ def textToRGB(text):
     nameAsciiDif = nameAsciiDif % 256
     nameAsciiMult = nameAsciiMult % 256
     return (nameAsciiSum, nameAsciiDif, nameAsciiMult)
+
+def getRGBFromText():
+    name = input("What is your name?\n")
+    textToRGB(name)
